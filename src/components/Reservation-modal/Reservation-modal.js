f849701a7f211fc6b957d791f5ebb0ab41cb5a13
alexandreleriche141282 <alexandreleriche141282@gmail.com>
2024-12-28 09:44:27 +0100

    crea modal + emailJS

--- src/components/Reservation-modal/Reservation-modal.js
+++ src/components/Reservation-modal/Reservation-modal.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { loadStripe } from '@stripe/stripe-js';
 import styles from './ChildcareReservationModal.module.scss';
+import emailjs from 'emailjs-com';
+
 
 const API_KEY = 'a9463cb081434344b0a3e1e0ab8b5a33';
 const ERAGNY_COORDINATES = { lat: 49.0139, lng: 2.1003 };
@@ -28,8 +30,9 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, selectedService }) => {
     specialNeeds: '',
     paymentMethod: '',
     clientType: '', // Ajouter ce nouveau champ
-  companyName: '', // Pour les entreprises
-  siret: '', // Pour les entreprises
+    companyName: '', // Pour les entreprises
+    siret: '', // Pour les entreprises
+    totalPrice: 0,
   });
   const [addressError, setAddressError] = useState('');
   const [timeError, setTimeError] = useState('');
@@ -37,29 +40,37 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, selectedService }) => {
 
   const calculatePrice = (startTime, endTime) => {
     if (!startTime || !endTime) return 0;
-  
+
     const [startHours, startMinutes] = startTime.split(':').map(Number);
     const [endHours, endMinutes] = endTime.split(':').map(Number);
-    
+
     const startInMinutes = startHours * 60 + startMinutes;
     const endInMinutes = endHours * 60 + endMinutes;
-  
+
     if (endInMinutes <= startInMinutes) return 0;
-  
+
     const durationInHours = (endInMinutes - startInMinutes) / 60;
     const price = Math.ceil(durationInHours * HOURLY_RATE);
-  
+
     console.log(`Start Time: ${startTime}, End Time: ${endTime}, Duration in Hours: ${durationInHours}, Price: ${price}`);
-    
+
     return price;
   };
-  
+
 
   const getTomorrow = () => {
     const tomorrow = new Date();
     tomorrow.setDate(tomorrow.getDate() + 1);
     return tomorrow.toISOString().split('T')[0];
   };
+
+  const formatDate = (dateString) => {
+    if (!dateString) return '';
+    const [year, month, day] = dateString.split('-');
+    return `${day}/${month}/${year}`;
+  };
+
+
 
   const questions = selectedService === 'Aide ménagère' ? [
     {
@@ -197,8 +208,8 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, selectedService }) => {
       endTime: '',
       specialNeeds: '',
       clientType: '',
-    companyName: '',
-    siret: '',
+      companyName: '',
+      siret: '',
     });
     setIsNextButtonEnabled(false);
     setAddressError('');
@@ -213,30 +224,44 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, selectedService }) => {
     setIsNextButtonEnabled(isStepValid());
   }, [formData, step]);
 
+  useEffect(() => {
+    emailjs.init("SPeSZLBDJLWstqlcr");
+  }, []);
+
+
   const handleChange = (e) => {
     const { name, value } = e.target;
-    
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value,
-      ...(name === 'paymentMethod' && value !== 'sap' ? { companyName: '', siret: '' } : {})
-    }));
-  
+    if (name === 'guardDate') {
+      const [day, month, year] = value.split('/');
+      const isoDate = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+      setFormData(prevState => ({
+        ...prevState,
+        [name]: value,
+        guardDateISO: isoDate
+      }));
+    } else {
+      setFormData(prevState => ({
+        ...prevState,
+        [name]: value,
+        ...(name === 'paymentMethod' && value !== 'sap' ? { companyName: '', siret: '' } : {})
+      }));
+    }
+
     if (name === 'startTime' || name === 'endTime') {
       validateTime(name, value);
-      
+
       const newPrice = calculatePrice(
         name === 'startTime' ? value : formData.startTime,
         name === 'endTime' ? value : formData.endTime
       );
-      
+
       setTotalPrice(newPrice);
-      
+
       // Log the updated total price
       console.log(`Updated Total Price: ${newPrice}`);
     }
   };
-  
+
 
   const validateTime = (field, time) => {
     const [hours, minutes] = time.split(':');
@@ -335,11 +360,12 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, selectedService }) => {
         body: JSON.stringify({
           serviceName: selectedService,
           totalPrice: totalPrice,
+          ...formData, // Inclure toutes les données du formulaire
         }),
       });
-      
+  
       const { id } = await response.json();
-      
+  
       const stripe = await stripePromise;
       const { error } = await stripe.redirectToCheckout({
         sessionId: id
@@ -353,32 +379,54 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, selectedService }) => {
     }
   };
   
-  
-  
+
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  if (isStepValid()) {
-    if (formData.paymentMethod === 'card') {
-      await handleStripePayment();
-      } else {
-        try {
-          const response = await axios.post('https://formspree.io/f/xanygezw', formData);
-          if (response.status === 200) {
-            setShowConfirmation(true);
+    if (isStepValid()) {
+      try {
+        const formattedChildrenDetails = formData.childrenDetails.map((child, index) =>
+          `Enfant ${index + 1} : Prénom - ${child.prenom}, Nom - ${child.nom}, Age - ${child.age}`
+        ).join('\n');
+  
+        const templateParams = {
+          ...formData,
+          formattedChildrenDetails,
+          selectedService,
+          totalPrice
+        };
+  
+        // Envoyer l'email de confirmation
+        const response = await emailjs.send("service_r0hs8xr", "template_gg6ezb8", templateParams);
+        
+        if (response.status === 200) {
+          // Vérifier le mode de paiement
+          if (formData.paymentMethod === 'cash') {
+            setShowConfirmationModal(true); // Afficher la modal de confirmation pour paiement en espèces
             setTimeout(() => {
-              setShowConfirmation(false);
-              onSubmit({ service: selectedService, ...formData });
-              onClose();
+              setShowConfirmationModal(false);
+              onClose(); // Fermer la modal principale après un délai
             }, 3000);
-          } else {
-            console.error('Erreur d\'envoi vers Formspree:', response);
+          } else if (formData.paymentMethod === 'card') {
+            await handleStripePayment(); // Rediriger vers Stripe pour le paiement par carte
           }
-        } catch (error) {
-          console.error('Erreur lors de l\'envoi vers Formspree:', error);
+        } else {
+          console.error('Erreur d\'envoi avec EmailJS:', response);
         }
+      } catch (error) {
+        console.error('Erreur lors de la soumission:', error);
       }
     }
+  };
+  
+
+  
+  
+  const handleCloseConfirmation = () => {
+    setShowConfirmationModal(false);
+    onClose();
   };
 
   const isStepValid = () => {
@@ -436,6 +484,15 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, selectedService }) => {
   };
 
   if (!isOpen) return null;
+  const ConfirmationModal = ({ onClose }) => (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalContent}>
+        <h2>Confirmation de réservation</h2>
+        <p>Votre demande de réservation a bien été prise en compte !</p>
+        <button onClick={onClose}>Fermer</button>
+      </div>
+    </div>
+  );
 
   const currentQuestion = questions[step];
 
@@ -444,11 +501,7 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, selectedService }) => {
       <div className={styles.modalContent}>
         <button className={styles.closeButton} onClick={onClose}>✖</button>
         <h2>Réservation - {selectedService}</h2>
-        {showConfirmation && (
-          <div className={styles.confirmationMessage}>
-            <p>Votre demande de réservation a bien été prise en compte !</p>
-          </div>
-        )}
+        {showConfirmationModal && <ConfirmationModal onClose={handleCloseConfirmation} />}
         <form onSubmit={handleSubmit}>
           <h3>{currentQuestion.title}</h3>
           {currentQuestion.fields.map((field, index) => {
@@ -483,15 +536,31 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, selectedService }) => {
                     onChange={handleChange}
                   />
                 ) : field.type === 'date' ? (
-                  <input
-                    id={field.name}
-                    type="date"
-                    name={field.name}
-                    value={formData[field.name]}
-                    onChange={handleChange}
-                    min={getTomorrow()}
-                    required
-                  />
+                  <div>
+                    <input
+                      id={field.name}
+                      type="date"
+                      name={field.name}
+                      value={formData[`${field.name}ISO`] || ''}
+                      onChange={(e) => {
+                        const isoDate = e.target.value;
+                        const formattedDate = formatDate(isoDate);
+                        handleChange({
+                          target: {
+                            name: field.name,
+                            value: formattedDate
+                          }
+                        });
+                        setFormData(prevState => ({
+                          ...prevState,
+                          [`${field.name}ISO`]: isoDate
+                        }));
+                      }}
+                      min={getTomorrow()}
+                      required
+                    />
+                    <span>{formData[field.name]}</span>
+                  </div>
                 ) : field.type === 'time' ? (
                   <input
                     id={field.name}
@@ -516,6 +585,7 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, selectedService }) => {
                   />
                 )}
               </div>
+
             );
           })}
           {currentQuestion.fields.some(field => field.type === 'time') && formData.startTime && formData.endTime && !timeError && (
@@ -544,19 +614,19 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, selectedService }) => {
               </button>
             ) : (
               <button
-  type="submit"
-  className={`submitButton ${!isNextButtonEnabled ? 'disabled' : 'enabled'}`}
-  disabled={!isNextButtonEnabled}
->
-  {formData.paymentMethod === 'card' ? 'Payer et Réserver' : 'Réserver'}
-</button>
+                type="submit"
+                className={`submitButton ${!isNextButtonEnabled ? 'disabled' : 'enabled'}`}
+                disabled={!isNextButtonEnabled}
+              >
+                {formData.paymentMethod === 'card' ? 'Réserver et procéder au paiement' : 'Réserver'}
+              </button>
             )}
           </div>
         </form>
       </div>
     </div>
   );
-  
+
 };
 
 export default ReservationModal;
